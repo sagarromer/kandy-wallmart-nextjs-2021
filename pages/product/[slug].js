730f3ebc90494b6f1d06d56bc5b4fbbe064e65a1
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-15 09:22:54 -0700

    fetch products from mongodb

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,3 +1,4 @@
+import React from 'react';
 import NextLink from 'next/link';
 import Image from 'next/image';
 import {
@@ -9,18 +10,16 @@ Typography,
 Card,
 Button,
 } from '@material-ui/core';
-import { useRouter } from 'next/router';
-import data from '../../utils/data';
 import Layout from '../../components/Layout';
 import useStyles from '../../utils/styles';
+import Product from '../../models/Product';
+import db from '../../utils/db';
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+    const { product } = props;
     const classes = useStyles();
-    const router = useRouter();
-    const { slug } = router.query;
-    const product = data.products.find((a) => a.slug === slug);
     if (!product) {
-    return <div>Product Not Found</div>;
+      return <div>Product Not Found</div>;
     }
     return (
         <Layout title={product.name} description={product.description}>
@@ -31,71 +30,86 @@ export default function ProductScreen() {
                     </Link>
                 </NextLink>
             </div>
-            <Grid container spacing={1}>
-                <Grid item md={6} xs={12}>
-                    <Image
+        <Grid container spacing={1}>
+            <Grid item md={6} xs={12}>
+                <Image
                     src={product.image}
                     alt={product.name}
                     width={640}
                     height={640}
                     layout="responsive"
-                    ></Image>
-                </Grid>
-                <Grid item md={3} xs={12}>
+                ></Image>
+            </Grid>
+            <Grid item md={3} xs={12}>
+                <List>
+                    <ListItem>
+                        <Typography component="h1" variant="h1">
+                            {product.name}
+                        </Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography>Category: {product.category}</Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography>Brand: {product.brand}</Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography>
+                            Rating: {product.rating} stars ({product.numReviews} reviews)
+                        </Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography> Description: {product.description}</Typography>
+                    </ListItem>
+                </List>
+            </Grid>
+            <Grid item md={3} xs={12}>
+                <Card>
                     <List>
                         <ListItem>
-                            <Typography component="h1" variant="h1">{product.name}</Typography>
+                            <Grid container>
+                                <Grid item xs={6}>
+                                    <Typography>Price</Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Typography>${product.price}</Typography>
+                                </Grid>
+                            </Grid>
                         </ListItem>
                         <ListItem>
-                            <Typography>Category: {product.category}</Typography>
+                            <Grid container>
+                                <Grid item xs={6}>
+                                    <Typography>Status</Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Typography>
+                                        {product.countInStock > 0 ? 'In stock' : 'Unavailable'}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
                         </ListItem>
                         <ListItem>
-                            <Typography>Brand: {product.brand}</Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Typography>
-                            Rating: {product.rating} stars ({product.numReviews} reviews)
-                            </Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Typography> Description: {product.description}</Typography>
+                            <Button fullWidth variant="contained" color="primary">
+                                Add to cart
+                            </Button>
                         </ListItem>
                     </List>
-                </Grid>
-                <Grid item md={3} xs={12}>
-                    <Card>
-                        <List>
-                            <ListItem>
-                                <Grid container>
-                                    <Grid item xs={6}>
-                                        <Typography>Price</Typography>
-                                    </Grid>
-                                    <Grid item xs={6}>
-                                        <Typography>${product.price}</Typography>
-                                    </Grid>
-                                </Grid>
-                            </ListItem>
-                            <ListItem>
-                                <Grid container>
-                                    <Grid item xs={6}>
-                                        <Typography>Status</Typography>
-                                    </Grid>
-                                    <Grid item xs={6}>
-                                        <Typography>
-                                            {product.countInStock > 0 ? 'In stock' : 'Unavailable'}
-                                        </Typography>
-                                    </Grid>
-                                </Grid>
-                            </ListItem>
-                            <ListItem>
-                                <Button fullWidth variant="contained" color="primary">
-                                    Add to cart
-                                </Button>
-                            </ListItem>
-                        </List>
-                    </Card>
-                </Grid>
+                </Card>
+            </Grid>
         </Grid>
-    </Layout>
-);
+        </Layout>
+    );
+}
+export async function getServerSideProps(context) {
+    const { params } = context;
+    const { slug } = params;
+
+    await db.connect();
+    const product = await Product.findOne({ slug }).lean();
+    await db.disconnect();
+    return {
+        props: {
+        product: db.convertDocToObj(product),
+        },
+    };
 }
