f146a362140b14bc579ed9023df58aae8f66d7d1
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-15 00:09:09 -0700

    created react context

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import Head from 'next/head'
 import NextLink from 'next/link';
 import {
@@ -10,10 +10,15 @@ Link,
 createTheme,
 ThemeProvider,
 CssBaseline,
+Switch
 } from '@material-ui/core';
 import useStyles from '../utils/styles';
+import { Store } from '../utils/Store';
+import Cookies from 'js-cookie';
 
 function Layout({title, description, children}) {
+    const { state, dispatch } = useContext(Store);
+    const { darkMode } = state;
     const theme = createTheme({typography: {
         h1: {
             fontSize: '1.6rem',
@@ -27,7 +32,7 @@ function Layout({title, description, children}) {
         },
         },
         palette: {
-            type: 'light',
+            type: darkMode ? 'dark' : 'light',
             primary: {
                 main: '#f04',
             },
@@ -37,6 +42,11 @@ function Layout({title, description, children}) {
         },
     });
     const classes = useStyles();
+    const darkModeChangeHandler = () => {
+        dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+        const newDarkMode = !darkMode;
+        Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+    };
     return (
         <div>
             <Head>
@@ -54,6 +64,10 @@ function Layout({title, description, children}) {
                         </NextLink>
                         <div className={classes.grow}></div>
                         <div>
+                            <Switch
+                                checked={darkMode}
+                                onChange={darkModeChangeHandler}
+                            ></Switch>
                             <NextLink href="/cart" passHref>
                                 <Link>Cart</Link>
                             </NextLink>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import '../styles/globals.css';
+import { StoreProvider } from '../utils/Store';
+
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -8,7 +10,11 @@ function MyApp({ Component, pageProps }) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
-  return <Component {...pageProps} />;
+  return   (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
 
 export default MyApp;
