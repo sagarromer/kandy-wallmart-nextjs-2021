de86fdb6fd0ead265d4a8762e77f2477bcdeaf69
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-16 21:10:20 -0700

    create login page completed

--- pages/cart.js
+++ pages/cart.js
@@ -22,8 +22,10 @@ import {
     ListItem,
 } from '@material-ui/core';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 
 function CartScreen() {
+    const router = useRouter();
     const { state, dispatch } = useContext(Store);
     const {
         cart: { cartItems },
@@ -39,7 +41,9 @@ function CartScreen() {
     const removeItemHandler = (item) => {
         dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
     };
-
+    const checkoutHandler = () => {
+        router.push('/shipping');
+    };
     return (
         <Layout title="Shopping Cart">
         <Typography component="h1" variant="h1">
@@ -128,7 +132,12 @@ function CartScreen() {
                                 </Typography>
                             </ListItem>
                             <ListItem>
-                                <Button variant="contained" color="primary" fullWidth>
+                                <Button
+                                    onClick={checkoutHandler}
+                                    variant="contained"
+                                    color="primary"
+                                    fullWidth
+                                    >
                                 Check Out
                                 </Button>
                             </ListItem>
