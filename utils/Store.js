40eefc8f9a84dcf34c20e89c3947b5de6ea4c883
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-25 10:04:32 -0700

    implemented place order action

--- utils/Store.js
+++ utils/Store.js
@@ -56,6 +56,8 @@ function reducer(state, action) {
             ...state,
             cart: { ...state.cart, paymentMethod: action.payload },
         };
+    case 'CART_CLEAR':
+        return { ...state, cart: { ...state.cart, cartItems: [] } };
     case 'USER_LOGIN':
         return { ...state, userInfo: action.payload };
     case 'USER_LOGOUT':
