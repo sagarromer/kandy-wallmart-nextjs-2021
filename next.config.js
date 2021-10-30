b9e529ba29283c2d41b9c2fea8d9b15e9633cbd5
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-30 08:37:44 -0700

    nextjs config changed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   reactStrictMode: true,
   images: { domains: ['res.cloudinary.com'] },
+  webpack: (config, {
+    isServer
+  }) => {
+    if (!isServer) {
+       // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+        config.resolve.fallback = {
+          fs: false
+        }
+    }
+
+    return config;
+  }
 }
